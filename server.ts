eb60bd3a016557c5ee23c549cd1f56d93f35ac88
Martin Kopecký <kopecky@thekrew.app>
2023-11-28 17:57:44 +0100

    V1.0.0

--- server.ts
+++ server.ts
@@ -4,6 +4,8 @@ const wss = new WebSocket.Server({ port: 8080 })
 
 const Clients = []
 
+console.log('Server Online on port 8080')
+
 function AllButOne(message: string, ws: WebSocket) {
   wss.clients.forEach((client) => {
     if (client !== ws) {
@@ -13,15 +15,30 @@ function AllButOne(message: string, ws: WebSocket) {
 }
 
 wss.on('connection', (ws) => {
-  ws.on('message', (message) => {
-    const jsonData = JSON.parse(message.toString())
-    if (jsonData.type === 'joined') {
+  ws.on('message', (msg) => {
+    const jsonData = JSON.parse(msg.toString())
+    console.log(jsonData)
+    if (jsonData.type == 'joined') {
+      if (jsonData.id == undefined) {
+        const index = Clients.findIndex((client) => client.ws === ws)
+        const client = Clients[index]
+        Clients.splice(index, 1)
+        const message = {
+          type: 'left',
+          name: client.name,
+        }
+        AllButOne(JSON.stringify(message), ws)
+        return
+      }
       Clients.push({ id: jsonData.id, name: jsonData.name, ws: ws })
       const message = {
         type: 'joined',
         name: jsonData.name,
       }
       AllButOne(JSON.stringify(message), ws)
+    }
+    if (jsonData.type == 'message') {
+      AllButOne(JSON.stringify(jsonData), ws)
     }
   })
 
